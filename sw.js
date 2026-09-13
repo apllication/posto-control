df6134f8e197507ae019c3cb7affd5b65f670dac
apllication <apps.mav@outlook.com>
2026-09-13 00:22:46 -0300

    Atualizar cache para nova pagina de administracao

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'posto-control-v3';
+const CACHE = 'posto-control-v4';
 const ASSETS = [
-  './', './controle-posto.html', './painel-gerente.html', './painel-supervisor.html',
+  './', './controle-posto.html', './painel-gerente.html', './painel-supervisor.html', './administracao-supervisor.html', './gerentes-postos.html',
   './manifest-gerente.webmanifest', './manifest-supervisor.webmanifest', './icone-posto.svg'
 ];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
