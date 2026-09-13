e329f8bc53ee80d63a1a7ca2ee087461b0658457
apllication <apps.mav@outlook.com>
2026-09-13 00:37:20 -0300

    Atualiza cache do PWA para remover atalhos do painel do supervisor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'posto-control-v5';
+const CACHE = 'posto-control-v6';
 const ASSETS = [
   './', './controle-posto.html', './painel-gerente.html', './painel-supervisor.html', './administracao-supervisor.html', './gerentes-postos.html',
   './manifest-gerente.webmanifest', './manifest-supervisor.webmanifest', './icone-posto.svg'
