00f354e47a9ee43ca9048abae3610b4e8f23bdf9
apllication <apps.mav@outlook.com>
2026-09-12 23:59:45 -0300

    Atualizar cache para nova versao do app

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'posto-control-v2';
+const CACHE = 'posto-control-v3';
 const ASSETS = [
   './', './controle-posto.html', './painel-gerente.html', './painel-supervisor.html',
   './manifest-gerente.webmanifest', './manifest-supervisor.webmanifest', './icone-posto.svg'
 ];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
